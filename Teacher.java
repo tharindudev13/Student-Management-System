f68f78aeede80a26305cea38812ab25393a8354b
td813 <tharindukodippli13@gmail.com>
2025-11-23 10:33:25 +0530

    Uptaded update person method

--- Teacher.java
+++ Teacher.java
@@ -1,12 +1,32 @@
 
 public class Teacher extends Person{
     String subject;
+    private int workDays;
+    double salary;
 
 
     public Teacher(String name, int age, String Id, String Email, String subject) {
         super(name, age, Id, Email);
         this.subject = subject;
     }
+
+    private double Salary(){
+        double bonus = 0;
+        double basic = workDays * 1500;
+        if(workDays > 20){
+            bonus = basic * 0.25;
+        }
+
+        salary = basic + bonus;
+        return salary;
+    }
+
+    
+
+    
+
+    
+
     @Override
     void showDetails(){
         super.showDetails();
@@ -17,6 +37,26 @@ public class Teacher extends Person{
 
 
     }
+
+    void updateTeacher(String id){
+        if (this.Id.equals(id)) {
+
+            System.out.print("Enter new Subject: ");
+            this.subject = scanner.nextLine();
+
+            System.out.print("Enter Worked Days: ");
+            this.workDays = scanner.nextInt();
+
+            this.salary = Salary();
+
+            System.out.println("Teacher details updated successfully.");
+        } else {
+            System.out.println("Teacher ID not found.");
+        }
+
+    }
+
+    
 
     @Override
     void show(){
